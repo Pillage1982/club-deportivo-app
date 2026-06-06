29d96052f332cd4fd9340059421869489f704f2b
Mario Riquelme <new.rol.master@gmail.com>
2026-06-06 19:34:30 -0400

    Make MySQL queries resilient to closed connections

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,25 +2,25 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error conectando a MySQL:', err.message);
     return;
   }
 
   console.log('Conexion MySQL correcta');
+  connection.release();
 });
 
-connection.on('error', (err) => {
-  console.error('Error MySQL:', err.message);
-});
-
-module.exports = connection;
+module.exports = pool;
