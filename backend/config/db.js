f5b4f4775d28c15739326c90bcd9699473321941
Mario Riquelme <new.rol.master@gmail.com>
2026-06-07 21:01:27 -0400

    Use MySQL connection pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,8 +10,51 @@ const pool = mysql.createPool({
   charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
+
+function query(sql, params, callback) {
+  if (typeof params === 'function') {
+    callback = params;
+    params = [];
+  }
+
+  const run = (attempt = 0) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        return callback(err);
+      }
+
+      connection.query(sql, params, (queryErr, results, fields) => {
+        if (queryErr) {
+          const shouldRetry =
+            attempt === 0 &&
+            (
+              queryErr.fatal ||
+              queryErr.code === 'ECONNRESET' ||
+              queryErr.code === 'PROTOCOL_CONNECTION_LOST' ||
+              queryErr.message.includes('closed state')
+            );
+
+          connection.destroy();
+
+          if (shouldRetry) {
+            return run(1);
+          }
+
+          return callback(queryErr);
+        }
+
+        connection.release();
+        callback(null, results, fields);
+      });
+    });
+  };
+
+  run();
+}
 
 pool.getConnection((err, connection) => {
   if (err) {
@@ -23,4 +66,6 @@ pool.getConnection((err, connection) => {
   connection.release();
 });
 
-module.exports = pool;
+module.exports = {
+  query
+};
