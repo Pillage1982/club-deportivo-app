500999dc6b2a41aa92a914278d09cce0b0adebe2
Mario Riquelme <new.rol.master@gmail.com>
2026-06-08 20:17:21 -0400

    Hide restricted module titles by tesorero

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -152,19 +152,27 @@ function aplicarRolesFrontend() {
   }
 
   if (rol === 'tesorero') {
-    ocultarSelector('.nav-asistencias');
+  ocultarSelector('.nav-asistencias');
 
-    ocultarElemento('modulo_asistencia');
-    ocultarElemento('modulo_eventos');
-    ocultarElemento('tabla_eventos_wrapper');
-    ocultarElemento('form_personas');
+  ocultarElemento('modulo_asistencia');
+  ocultarElemento('modulo_eventos');
+  ocultarElemento('tabla_eventos_wrapper');
+  ocultarElemento('form_personas');
+  ocultarElemento('titulo_personas');
+  ocultarElemento('asistencias');
 
-    document
+  ocultarElementos([
+    'titulo_registrar_asistencia',
+    'titulo_asistencias',
+    'titulo_eventos'
+  ]);
+
+  document
     .querySelectorAll('#tabla_personas button')
     .forEach(btn => {
       btn.style.display = 'none';
     });
-  }
+}
 
   if (rol === 'entrenador') {
     ocultarSelector('.nav-multas');
