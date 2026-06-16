53a841a01be88f9e2dcf0a79aab261f7a060aa33
Mario Riquelme <new.rol.master@gmail.com>
2026-06-16 11:11:24 -0400

    Agrega configuracion base por cliente

---
+++ frontend/js/config.js
@@ -0,0 +1,35 @@
+window.APP_CONFIG = {
+  producto: {
+    nombre: 'NexoComunidad',
+    descripcion: 'Sistema administrativo adaptable para organizaciones'
+  },
+
+  cliente: {
+    nombre: 'Gran Diablada Calameña',
+    nombreCorto: 'Diablada Calameña',
+    fundacion: 'Fundada el 22 de octubre de 1961',
+    logo: 'img/logo-calamena.jpeg',
+    logoOscuro: 'img/logo-calamena-black.jpeg'
+  },
+
+  textos: {
+    integrantes: 'Integrantes',
+    actividades: 'Actividades',
+    asistencia: 'Asistencia',
+    finanzas: 'Finanzas',
+    cuotas: 'Cuotas',
+    multas: 'Multas'
+  },
+
+  rolesVisuales: {
+    admin: 'Administrador',
+    tesorero: 'Tesorero',
+    entrenador: 'Encargado'
+  },
+
+  tiposActividad: {
+    entrenamiento: 'Ensayo',
+    partido: 'Presentacion',
+    reunion: 'Reunion'
+  }
+};
